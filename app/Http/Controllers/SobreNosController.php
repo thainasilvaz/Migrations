5eb5ebf7238d7cf9d12ca45a3625b8297fa5bb2c
thainasilvaz <thaina.ferreira026@gmail.com>
2023-09-05 10:53:53 -0300

    views e controllers

---
+++ app/Http/Controllers/SobreNosController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SobreNosController extends Controller
+{
+    public function principal(){
+
+        echo 'controller sobre nós';
+        
+        }
+}
---
+++ resources/views/site/principal.blade.php
@@ -0,0 +1,1 @@
+<h3>PRINCIPAL(view)</h3>
